eee1c4d23cb35610728c36b8cb7033f40aee9a03
H2Orizon <ferents1vladeslav@gmail.com>
2024-10-16 17:27:14 +0300

    add templayes

--- app.py
+++ app.py
@@ -1,16 +1,15 @@
-from flask import Flask, request, redirect, url_for
+from flask import Flask, request, redirect, url_for, render_template
 app = Flask(__name__)
 app.config.from_pyfile("config.py")
 
 @app.route('/')
 def main():
-    return 'Hello, world'
+    return render_template("main.html")
 
 @app.route('/homepage') 
 def home():
-    """View for the Home page of your website."""
     agent = request.user_agent
-    return f"This is your homepage :) - {agent} "
+    return render_template("home.html", agent=agent)
 
 @app.route('/hi/<string:name>') 
 def hi_someone(name):
@@ -23,6 +22,10 @@ def admin():
     to_url = url_for("hi_someone",name = "administrator", _external=True)
     print(to_url)
     return redirect(to_url)
+
+@app.route('/about') 
+def about():
+    return render_template('about.html', company_name='Test')
 
 if __name__ == '__main__':
     app.run(debug = True)
