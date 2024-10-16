dd07ffaf4379fe01e7f8e9f5c54454588875d7ec
H2Orizon <ferents1vladeslav@gmail.com>
2024-10-16 18:31:22 +0300

    add templaye - detail_post, post,base

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, redirect, url_for, render_template
+from flask import Flask, request, redirect, url_for, render_template,abort
 app = Flask(__name__)
 app.config.from_pyfile("config.py")
 
@@ -12,20 +12,33 @@ def home():
     return render_template("home.html", agent=agent)
 
 @app.route('/hi/<string:name>') 
-def hi_someone(name):
+def hi(name):
     age = request.args.get("age", None, type=int)
-    name = name.upper()
-    return f"Welcome {name=} {age=}"
+    return render_template("hi.html", name=name, age=age)
 
 @app.route('/admin') 
 def admin():
-    to_url = url_for("hi_someone",name = "administrator", _external=True)
+    to_url = url_for("hi",name = "administrator", _external=True)
     print(to_url)
     return redirect(to_url)
 
-@app.route('/about') 
-def about():
-    return render_template('about.html', company_name='Test')
+posts = [
+    {"id": 1, 'title': 'My First Post', 'content': 'This is the content of my first post.', 'author': 'John Doe'},
+    {"id": 2, 'title': 'Another Day', 'content': 'Today I learned about Flask macros.', 'author': 'Jane Smith'},
+    {"id": 3, 'title': 'Flask and Jinja2', 'content': 'Jinja2 is powerful for templating.', 'author': 'Mike Lee'}
+]
+
+@app.route('/posts') 
+def get_posts():
+    return render_template("post.html", posts=posts)
+
+@app.route('/post/<int:id>') 
+def get_detail_posts(id):
+    post = posts[id-1]
+    if id > 3:
+        abort(404)
+    return render_template("detail_post.html", post=post)
+
 
 if __name__ == '__main__':
     app.run(debug = True)
