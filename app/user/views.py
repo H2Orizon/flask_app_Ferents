5306f42e76c5abad6e7b171690e055e10dd037f3
H2Orizon <ferents1vladeslav@gmail.com>
2024-10-30 18:27:21 +0200

    add session

--- app/user/views.py
+++ app/user/views.py
@@ -1,6 +1,26 @@
-from flask import request, redirect, url_for,render_template, make_response
+from flask import request, session, redirect, url_for,render_template, make_response
 from . import user_bp
 from datetime import timedelta, datetime
+
+@user_bp.route("/profile")
+def get_profile():
+    if "user" in session:
+        user_name = session["user"]
+        return render_template("user/profile.html", user_name=user_name)
+    return redirect(url_for("user.login"))
+
+@user_bp.route("/login", methods=['GET','POST'])
+def login():
+    if request.method == "POST":
+        username = request.form.get("login")
+        session["user"] = username
+        return redirect(url_for("user.get_profile"))
+    return render_template("user/logIn.html")
+
+@user_bp.route("/logout")
+def logout():
+    session.pop("user",None)
+    return redirect(url_for('user.get_profile'))
 
 @user_bp.route('/<string:name>') 
 def hi(name):
@@ -15,14 +35,16 @@ def admin():
 
 @user_bp.route('/set_cookie')
 def set_cookie():
-    response = make_response('Кука')
+    response = make_response('Кука створина')
     #response.set_cookie('user','student',expire=datetime.now()+timedelta(seconds=10))
     response.set_cookie('user','student',max_age=timedelta(seconds=10))
     return response
+
 @user_bp.route('/get_cookie')
 def get_cookie():
     username = request.cookies.get('user')
     return f'Користувач: {username}'
+
 @user_bp.route('/delete_cookie')
 def delete_cookie():
     response =make_response('del')
